ac4bfe040adb703e305c59d5feaeb1e898bc1249
Eleanor Atkinson <atkinson.el@northeastern.edu>
2023-11-02 18:21:05 -0400

    final commit to a3

--- src/Kanbas/Courses/Home/index.js
+++ src/Kanbas/Courses/Home/index.js
@@ -1,13 +1,89 @@
 import ModuleList from "../Modules/ModuleList";
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { SlTarget } from "react-icons/sl";
+import { BsBarChartLineFill } from "react-icons/bs";
+import { IoMegaphoneOutline } from "react-icons/io5";
+import { BiBell } from "react-icons/bi";
+import { PiEyeglassesLight } from "react-icons/pi";
+import "./index.css";
+import {BsFill5CircleFill} from "react-icons/bs";
+import {TiDeleteOutline} from "react-icons/ti";
 
 
 function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-      <ModuleList />
-      <h2>Status</h2>
-    </div>
-  );
+    return (
+        <div className="row">
+            <div className="col-8">
+                <h2>Home</h2>
+                <ModuleList />
+            </div>
+            <div className="col-3">
+                <h2>Status</h2>
+
+                <br></br>
+
+                <div className="student-view">
+                    <a href="#"><button type="button" class="btn btn-light btn-outline-secondary">
+                        <PiEyeglassesLight style={{ marginRight: "5px" }} />
+                        Student View </button></a>
+                </div>
+
+                <hr></hr>
+
+                <ul class="list-group button-list-group">
+                    <li class="list-group-item border-0"> <a href="#"><button type="button" class="btn btn-light btn-outline-secondary float-middle btn-black">
+                        <HiArrowRightOnRectangle />
+                        Import Existing Content</button></a> </li>
+                    <li class="list-group-item border-0"> <a href="#"><button type="button" class="btn btn-light btn-outline-secondary float-middle">
+                        <FaArrowRightFromBracket />
+                        Import from Commons</button></a> </li>
+                    <li class="list-group-item border-0"> <a href="#"><button type="button" class="btn btn-light btn-outline-secondary float-middle">
+                        <SlTarget />
+                        Choose Home Page</button></a> </li>
+                    <li class="list-group-item border-0"> <a href="#"><button type="button" class="btn btn-light btn-outline-secondary float-middle">
+                        <BsBarChartLineFill />
+                        View Course Stream</button></a> </li>
+                    <li class="list-group-item border-0"> <a href="#"><button type="button" class="btn btn-light btn-outline-secondary float-middle">
+                        <IoMegaphoneOutline />
+                        New Announcement</button></a> </li>
+                    <li class="list-group-item border-0"> <a href="#"><button type="button" class="btn btn-light btn-outline-secondary float-middle">
+                        <BsBarChartLineFill />
+                        New Analytics</button></a> </li>
+                    <li class="list-group-item border-0"> <a href="#"><button type="button" class="btn btn-light btn-outline-secondary float-middle">
+                        <BiBell />
+                        View Course Notifications</button></a> </li>
+
+
+                </ul>
+                <br></br>
+                <ul>
+                    <b>To Do</b>
+                    <div class="grey-text">
+                        <hr></hr>
+                        <ul class="list-unstyled"> 
+                            <li> 
+                                <BsFill5CircleFill style={{ color: "red", marginRight: "10px" }}/> 
+                                <a href="#">Grade A1 - ENV + HTML</a>
+                                <TiDeleteOutline style={{ marginLeft: "10px"}}/>
+                                <br></br>
+                                       100 points • Sep 18 at 11:59
+                            </li>
+                            <br></br>
+                            <li> 
+                                <BsFill5CircleFill style={{ color: "red", marginRight: "10px" }}/> 
+                                <a href="#">Grade A2 - CSS + BOOTSTRAP</a>
+                                <TiDeleteOutline style={{ marginLeft: "10px"}}/>
+                                <br></br>
+                                100 points • Oct 2 at 11:59
+                            </li>
+                            <br></br>
+                        </ul>
+                    </div>
+                </ul>
+
+            </div>
+        </div>
+    );
 }
 export default Home;
--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,32 +1,64 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
-import {IoMdCheckmarkCircle} from "react-icons/io";
+import { IoMdCheckmarkCircle } from "react-icons/io";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
+import {BsThreeDotsVertical} from "react-icons/bs"
 
 
 function ModuleList() {
-  const { courseId } = useParams();
-  const modules = db.modules;
-  return (
-    <ul className="list-group">
-      {
-       modules
-         .filter((module) => module.course === courseId)
-         .map((module, index) => (
-<ul key={index} className = "module-list-group">
-           <li key={index} className="list-group-item list-group-item-secondary">
-           <IoMdCheckmarkCircle  style={{ color: "green", float: 'right', }}/>
-             <h3>{module.name}</h3>
-             
-             </li>
-             <li key={index} className="list-group-item list-group-item-light">
-             <p>{module.description}</p>
-           </li>
-           </ul>
+    const { courseId } = useParams();
+    const modules = db.modules;
+    return (
+        <div>
+            <br></br>
+            <table class="button-table style={{float: 'right'}}">
+                <tbody>
+                    <tr>
+                        <th scope="row"></th>
+                        <td> <button type="button" class="btn btn-light btn-outline-secondary">Collapse All</button> </td>
+                        <td> <button type="button" class="btn btn-light btn-outline-secondary">View Progress</button> </td>
+                        <td> <div class="dropdown">
+                            <button type="button" class="btn dropdown-toggle btn-light btn-outline-secondary" data-toggle="dropdown">
+                                <IoCheckmarkCircleOutline style={{ color: "green" }} />
+                                Publish All
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Introduction to Rocket Propulsion</a>
+                                <a class="dropdown-item" href="#">Fuel and Combustion</a>
+                                <a class="dropdown-item" href="#">Nozzle Design</a>
+                            </div>
+                    
+                        </div>
+                        </td>
+                        <td> <button type="button" class="btn btn-danger">+ Module</button> </td>
+                        <td> <button type="button" class="btn btn-light btn-outline-secondary"> <BsThreeDotsVertical/></button> </td>
+                    </tr>
+                </tbody>
+            </table>
+            <hr/> 
+            <br></br>
 
-      ))
-      }
-    </ul>
-  );
+            <ul className="list-group">
+                {
+                    modules
+                        .filter((module) => module.course === courseId)
+                        .map((module, index) => (
+                            <ul key={index} className="module-list-group">
+                                <li key={index} className="list-group-item list-group-item-secondary">
+                                    <IoMdCheckmarkCircle style={{ color: "green", float: 'right', }} />
+                                    <h3>{module.name}</h3>
+
+                                </li>
+                                <li key={index} className="list-group-item list-group-item-light">
+                                    <p>{module.description}</p>
+                                </li>
+                            </ul>
+
+                        ))
+                }
+            </ul>
+        </div>
+    );
 }
 export default ModuleList;
