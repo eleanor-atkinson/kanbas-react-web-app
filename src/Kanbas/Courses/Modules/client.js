2a3115df1dc8f61a39fc03c6797026325726e26b
Eleanor Atkinson <atkinson.el@northeastern.edu>
2023-12-01 20:38:08 -0500

    final commit with routes working before deploying

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,10 @@
 import axios from "axios";
-const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+const API_BASE = "https://kanbas-node-server-app-vcsj.onrender.com/api"; 
+const MODULES_URL = `${API_BASE}/modules`;
+// const COURSES_URL = "http://localhost:4000/api/courses";
+const COURSES_URL = `${API_BASE}/courses`;
+console.log(MODULES_URL);
+console.log(COURSES_URL);
 
 export const createModule = async (courseId, module) => {
   const response = await axios.post(
@@ -11,18 +15,15 @@ export const createModule = async (courseId, module) => {
 };
 
 export const deleteModule = async (moduleId) => {
-  const response = await axios
-    .delete(`${MODULES_URL}/${moduleId}`);
+  const response = await axios.delete(`${MODULES_URL}/${moduleId}`);
   return response.data;
 };
 export const findModulesForCourse = async (courseId) => {
-  const response = await axios
-    .get(`${COURSES_URL}/${courseId}/modules`);
+  const response = await axios.get(`${COURSES_URL}/${courseId}/modules`);
   return response.data;
 };
 export const updateModule = async (module) => {
-    const response = await axios.
-      put(`${MODULES_URL}/${module._id}`, module);
+    const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
     return response.data;
   };
   
